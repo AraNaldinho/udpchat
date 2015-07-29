748835e9960aa34389bf609f0a411a5c87008b0b
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2015-07-29 21:39:31 +0530

    Replaced client-new with client-old

--- client.py
+++ client.py
@@ -7,33 +7,26 @@ try:
 except socket.error:
     print 'Failed to create socket'
     sys.exit()
-c2smsg=["initial",""]
-prevrply="initial"
-currrply="initial"
+ 
 host = '10.42.0.1';
 port = 8888;
-while(1):
-    c2smsg[0]=c2smsg[1]
-    c2smsg[1] = raw_input('Enter message to send : ')
-    if c2smsg[1]=="exit":
+ 
+while(1) :
+    msg = raw_input('Enter message to send : ')
+    if msg=="exit":
         s.sendto("client exitted", (host, port))
         break
      
     try :
         #Set the whole string
-        s.sendto(c2smsg[0], (host, port))
-        s.sendto(c2smsg[1], (host, port))
+        s.sendto(msg, (host, port))
          
         # receive data from client (data, addr)
-        s2cmsg0 = s.recvfrom(1024)
-        s2cmsg1 = s.recvfrom(1024)
-        prevrply = s2cmsg0[0]
-        if prevrply == currrply or prevrply=="initial":
-            currrply = s2cmsg1[0]
-        else:
-            currrply = s2cmsg0[0]
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
          
-        print 'Server reply : ' + currrply
+        print 'Server reply : ' + reply
      
     except socket.error, msg:
         print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
