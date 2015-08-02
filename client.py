13dc92d9601ff108395e9bd47d21c0a28237fd86
thangaram <thangaram611@gmail.com>
2015-08-02 19:16:17 +0530

    starting again from beginning
    used only the basic connection code

--- client.py
+++ client.py
@@ -4,20 +4,9 @@ IP = "127.0.0.1"
 PORT = 5005
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-c2s=["init0",""]
-prevrply="init1"
-s2c=["",""]
-
 while True:
-    c2s[1]=raw_input("client: ")
-    sock.sendto(c2s[0],(IP,PORT))
-    sock.sendto(c2s[1],(IP,PORT))
-    s2c[0],servaddr=sock.recvfrom(1024)
-    s2c[1],servaddr=sock.recvfrom(1024)
-    if s2c[0]==prevrply:
-        print 'Server:',s2c[1]
-        prevrply=s2c[1]
-    else:
-        print 'Server:',s2c[0] 
-        prevrply=s2c[0]
-    c2s[0]=c2s[1]
+	msg = raw_input("enter the word :")
+	sock.sendto(msg,(IP,PORT))
+	
+	meaning = sock.recvfrom(1024)
+	print "server reply :" + meaning
--- server.py
+++ server.py
@@ -1,24 +1,7 @@
 import socket
 
-IP = ""
+IP = "127.0.0.1"
 PORT = 5005
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((IP,PORT))
 
-s2c=["init1",""]
-prevrply="init0"
-c2s=["",""]
-
-while True:
-    c2s[0],cliaddr = sock.recvfrom(1024)
-    c2s[1],cliaddr = sock.recvfrom(1024)
-    if c2s[0]==prevrply:
-        print 'Client:',c2s[1]
-        prevrply=c2s[1]
-    else:
-        print 'Client:',c2s[0] 
-        prevrply=c2s[0]
-    s2c[1]=raw_input("Server: ")
-    sock.sendto(s2c[0],cliaddr)
-    sock.sendto(s2c[1],cliaddr)
-    s2c[0]=s2c[1]
